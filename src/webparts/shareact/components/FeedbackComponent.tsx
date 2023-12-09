5177056d4a6b24137c248fdac0ba2102e0c26202
Chandra Sekhar Malla <43902003+sekharmalla@users.noreply.github.com>
2023-12-09 10:38:35 +0000

    Introduction to Shareact

---
+++ src/webparts/shareact/components/FeedbackComponent.tsx
@@ -0,0 +1,17 @@
+// import * as React from 'react';
+// import  Picker from 'emoji-picker-react';
+
+// const FeedbackComponent = (props) => {
+    
+//     const onEmojiClick = (event, emojiObject) => {
+//         // Logic to handle emoji click
+//       };
+
+// return (
+//     <div>
+//         <Picker onEmojiClick={onEmojiClick}/>
+//     </div>
+// )
+// }
+
+// export default FeedbackComponent;
---
+++ src/webparts/shareact/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IShareactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ShareactWebPartStrings' {
+  const strings: IShareactWebPartStrings;
+  export = strings;
+}
